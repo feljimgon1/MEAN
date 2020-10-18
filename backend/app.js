94b4c359b66c06bfb2befb3ac3465cb955c31e9e
Félix <feljimgon1@alum.us.es>
2020-10-18 12:29:35 +0200

    [Backend]
    Tasks model finished (backend finished as well)

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,11 @@ app.use((req, res, next) => {
     next();
 });
 
+//LISTS
+
 app.get('/lists', (req, res)=>{
     List.find({})
-        .then(lists => res.send(lists))
+        .then((lists) => res.send(lists))
         .catch((error) => console.log(error));
 });
 
@@ -29,19 +31,58 @@ app.post('/lists', (req, res) => {
 
 app.get('/lists/:listId', (req, res) => {
     List.find({_id:req.params.listId})
-        .then(lists => res.send(lists))
+        .then((lists) => res.send(lists))
         .catch((error) => console.log(error));
 });
 
 app.patch('/lists/:listId', (req, res) => {
     List.findOneAndUpdate({'_id':req.params.listId}, {$set:req.body})
-        .then(lists => res.send(lists))
+        .then((lists) => res.send(lists))
         .catch((error) => console.log(error));
 });
 
 app.delete('/lists/:listId', (req, res) => {
-    List.findByIdAndDelete({'_id':req.params.listId})
-        .then(lists => res.send(lists))
+    const deleteTasks = (list) => {
+        Task.deleteMany({_listId: list._id})
+            .then(()=>list)
+            .catch((error) => console.log(error));
+    };
+    const list = List.findByIdAndDelete(req.params.listId)
+                    .then((list) => deleteTasks(list))
+                    .catch((error) => console.log(error));
+    res.status(200).send(list);
+});
+
+//Tasks
+
+app.get('/lists/:listId/tasks', (req, res) => {
+    Task.find({ _listId: req.params.listId })
+        .then((tasks) => res.send(tasks))
+        .catch((error) => console.log(error));
+});
+
+app.post('/lists/:listId/tasks', (req, res) => {
+    (new Task({ '_listId': req.params.listId, 'title': req.body.title }))
+        .save()
+        .then((tasks) => res.send(tasks))
+        .catch((error) => console.log(error));
+});
+
+app.get('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOne({ _listId: req.params.listId, _id: req.params.taskId })
+        .then((task) => res.send(task))
+        .catch((error) => console.log(error));
+});
+
+app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndUpdate({ _listId: req.params.listId, _id: req.params.taskId}, {$set: req.body})
+        .then((task) => res.send(task))
+        .catch((error) => console.log(error));
+});
+
+app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndDelete({ _listId: req.params.listId, _id: req.params.taskId})
+        .then((task) => res.send(task))
         .catch((error) => console.log(error));
 });
 
